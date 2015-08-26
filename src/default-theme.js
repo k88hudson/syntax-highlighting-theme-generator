5e3c197821c6fa111985f6f30eae9df3fc725497
Kate Hudson <k88hudson@gmail.com>
2015-08-26 11:32:20 -0400

    style

--- src/default-theme.js
+++ src/default-theme.js
@@ -16,3 +16,30 @@ module.exports = {
   highlightBackground: '#F7EBC6',
   highlightAccent: '#F7D87C'
 };
+
+module.exports = {
+  'Inline Code': {
+    inlineCodeColor: '#DB4C69',
+    inlineCodeBackground: '#F9F2F4'
+  },
+  'Code Blocks': {
+    blockBackground: '#F8F5EC',
+    baseColor: '#5C6E74',
+    selectedColor: '#b3d4fc'
+  },
+  'Line Highlights': {
+    highlightBackground: '#F7EBC6',
+    highlightAccent: '#F7D87C'
+  },
+  'Tokens': {
+    commentColor: '#93A1A1',
+    punctuationColor: '#999999',
+    propertyColor: '#990055',
+    selectorColor: '#669900',
+    operatorColor: '#a67f59',
+    operatorBg: '#FFFFFF',
+    variableColor: '#ee9900',
+    functionColor: '#DD4A68',
+    keywordColor: '#0077aa'
+  }
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,20 @@
 var React = require('react/addons');
 var {Markdown} = require('react-markdocs');
 var throttle = require('lodash.throttle');
+var typeset = require('typeset');
 
 var defaultTheme = require('./default-theme');
-var colorClasses = Object.keys(defaultTheme);
+var colorClasses = [];
+var allColors = {};
+
+Object.keys(defaultTheme).forEach(key => {
+  var group = defaultTheme[key];
+  Object.keys(group).forEach(color => {
+    colorClasses.push(color);
+    allColors[color] = group[color];
+  });
+});
+
 var lessString = require('raw!../node_modules/react-markdocs/src/markdocs.less');
 
 function createColorVariableString(colors) {
@@ -13,8 +24,10 @@ function createColorVariableString(colors) {
 }
 
 function renderStyles(colors, callback) {
+
   var str = lessString;
   str = str + '\n\n' + createColorVariableString(colors);
+
   window.less.render(str)
     .then(function (output) {
       callback(output.css);
@@ -39,7 +52,7 @@ var App = React.createClass({
   mixins: [React.addons.LinkedStateMixin],
   getInitialState: function () {
     return {
-      colors: defaultTheme,
+      colors: allColors,
       css: ''
     };
   },
@@ -54,6 +67,11 @@ var App = React.createClass({
     // this.updateColors();
 
   },
+  scrollToExample: function () {
+    var previewEl = this.refs.preview.getDOMNode();
+    var egTop = document.getElementById('examples').offsetTop;
+    if (previewEl.scrollTop < egTop) previewEl.scrollTop = egTop - 30;
+  },
   updateSwatch: function (color) {
     return (e) => {
       var colors = this.state.colors;
@@ -64,18 +82,26 @@ var App = React.createClass({
   },
   render: function () {
     return (<div className="container">
-      <div className="sidebar">
-        <div className="color-wrapper">{colorClasses.map(color => {
-          return <ColorInput label={color} value={this.state.colors[color]} onChange={this.updateSwatch(color)} />
+      <style>{this.state.css}</style>
+      <div className="sidebar" onClick={this.scrollToExample}>
+        <div hidden={!this.state.css}>
+          <a className="btn" href={'data:application/octet-stream;charset=utf-8,' + encodeURI(this.state.css)}>Download CSS</a>
+        </div>
+        <div className="color-wrapper">{Object.keys(defaultTheme).map(group => {
+          return (<div className="form-group">
+            <h3>{group}</h3>
+            {Object.keys(defaultTheme[group]).map(color => {
+              return <ColorInput label={color} value={this.state.colors[color]} onChange={this.updateSwatch(color)} />
+            })}
+          </div>);
         })}</div>
       </div>
-      <div className="preview">
-        <Markdown prism source={require('./docs/main.md')} />
+      <div ref="preview" className="preview">
+        <Markdown prism source={require('./docs/main.md')} options={{html: true}} postProcess={(html) => typeset(html, {ligatures: false})} />
       </div>
-      <footer className="download" hidden={this.state.css}>
+      <footer className="download" hidden={true}>
         <a className="btn" href={'data:application/octet-stream;charset=utf-8,' + encodeURI(this.state.css)}>Download CSS</a>
       </footer>
-      <style>{this.state.css}</style>
     </div>);
   }
 });
